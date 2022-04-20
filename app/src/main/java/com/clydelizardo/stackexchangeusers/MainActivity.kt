d6fb7e886c85f721e2139500cac6103862b44cc5
Clyde <box.magnet@gmail.com>
2022-04-20 16:59:18 +0100

    Update activity and theme for adding screens

--- app/src/main/java/com/clydelizardo/stackexchangeusers/MainActivity.kt
+++ app/src/main/java/com/clydelizardo/stackexchangeusers/MainActivity.kt
@@ -2,10 +2,15 @@ package com.clydelizardo.stackexchangeusers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.clydelizardo.stackexchangeusers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var contentView: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        contentView = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(contentView.toolbar)
     }
 }
