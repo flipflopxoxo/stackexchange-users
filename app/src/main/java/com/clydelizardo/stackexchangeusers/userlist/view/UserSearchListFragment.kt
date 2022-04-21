35c0bec317322f18bf889687020427eae9a9eec2
Clyde <box.magnet@gmail.com>
2022-04-21 14:16:10 +0100

    Bind view model to UserSearchListFragment

--- app/src/main/java/com/clydelizardo/stackexchangeusers/userlist/view/UserSearchListFragment.kt
+++ app/src/main/java/com/clydelizardo/stackexchangeusers/userlist/view/UserSearchListFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.clydelizardo.stackexchangeusers.databinding.FragmentUserSearchListBinding
+import com.clydelizardo.stackexchangeusers.userlist.viewmodel.UserSearchListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class UserSearchListFragment : Fragment() {
     private var binding: FragmentUserSearchListBinding? = null
+    private val viewModel: UserSearchListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +22,7 @@ class UserSearchListFragment : Fragment() {
     ): View {
         return FragmentUserSearchListBinding.inflate(inflater, container, false).also {
             binding = it
+            it.viewModel = viewModel
         }.root
     }
 
