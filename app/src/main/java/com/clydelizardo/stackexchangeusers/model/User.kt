d67bcbb0afa8cec33377b3b1d9e15432680fc571
Clyde <box.magnet@gmail.com>
2022-04-21 13:48:47 +0100

    Create User model

---
+++ app/src/main/java/com/clydelizardo/stackexchangeusers/model/User.kt
@@ -0,0 +1,12 @@
+package com.clydelizardo.stackexchangeusers.model
+
+import java.util.*
+
+data class User(
+    val id: Int,
+    val name: String,
+    val reputation: Int,
+    val topTags: List<String>,
+    val location: String,
+    val creationDate: Date,
+)
