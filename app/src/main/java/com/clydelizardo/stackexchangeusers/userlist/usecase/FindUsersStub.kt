868cc6b12e34e7efbc029b27799e8c137a45f708
Clyde <box.magnet@gmail.com>
2022-04-22 01:56:16 +0100

    Fix compilation issue caused by missing constructor value

--- app/src/main/java/com/clydelizardo/stackexchangeusers/userlist/usecase/FindUsersStub.kt
+++ app/src/main/java/com/clydelizardo/stackexchangeusers/userlist/usecase/FindUsersStub.kt
@@ -9,10 +9,10 @@ class FindUsersStub @Inject constructor(): FindUsers {
     override suspend fun invoke(query: String): List<User> {
         delay(500)
         return listOf(
-            User(id = 0, name = "mark", reputation = 1, topTags = emptyList(), location = "London, UK", creationDate = Date()),
-            User(id = 1, name = "bryan", reputation = 1, topTags = emptyList(), location = "London, UK", creationDate = Date()),
-            User(id = 2, name = "sam", reputation = 1, topTags = emptyList(), location = "London, UK", creationDate = Date()),
-            User(id = 3, name = "steve", reputation = 1, topTags = emptyList(), location = "London, UK", creationDate = Date()),
+            User(id = 0, name = "mark", reputation = 1, topTags = emptyList(), location = "London, UK", creationDate = Date(), profileImageUrl = ""),
+            User(id = 1, name = "bryan", reputation = 1, topTags = emptyList(), location = "London, UK", creationDate = Date(), profileImageUrl = ""),
+            User(id = 2, name = "sam", reputation = 1, topTags = emptyList(), location = "London, UK", creationDate = Date(), profileImageUrl = ""),
+            User(id = 3, name = "steve", reputation = 1, topTags = emptyList(), location = "London, UK", creationDate = Date(), profileImageUrl = ""),
         )
     }
 }
