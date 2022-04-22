771c04e1db8dbc52c037ca0f4156d6b3ab3c825f
Clyde <box.magnet@gmail.com>
2022-04-22 14:28:06 +0100

    Show user profile in user details screen

--- app/src/main/java/com/clydelizardo/stackexchangeusers/userdetails/view/UserDetailsFragment.kt
+++ app/src/main/java/com/clydelizardo/stackexchangeusers/userdetails/view/UserDetailsFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.clydelizardo.stackexchangeusers.R
 import com.clydelizardo.stackexchangeusers.databinding.FragmentUserDetailsBinding
 import com.clydelizardo.stackexchangeusers.userdetails.viewmodel.UserDetailsViewModel
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,7 +24,18 @@ class UserDetailsFragment: Fragment() {
     ): View? {
         return FragmentUserDetailsBinding.inflate(inflater, container, false).also{
             it.viewModel = viewModel
+            binding = it
         }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.profileImageView?.let {
+            Picasso.get()
+                .load(viewModel.profileImageUrl)
+                .placeholder(R.drawable.ic_baseline_person_256)
+                .into(it)
+        }
     }
 
     override fun onDestroyView() {
